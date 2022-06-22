7f4a21e7e700036139e2155efc0f689d6346643a
Inwebo <github@inwebo.net>
2022-06-22 16:29:00 +0200

    Switch to webpack 5

--- docs/assets/js/demos/demo-1.js
+++ docs/assets/js/demos/demo-1.js
@@ -1,17 +1,20 @@
 import {Entity} from "../../../../src";
 import {Vector2D} from "@inwebo/vector";
 import EntityRenderer from "../renderers/EntityRenderer";
+import Forces from "../../../../src/Kinematics/Forces";
 
 const demo1 = () => {
+    const rand = (a,b) => a+(b-a+1)*crypto.getRandomValues(new Uint32Array(1))[0]/2**32|0;
+
     const canvas   = document.getElementById('demo-1');
     const entity   = new Entity(new Vector2D(320, 240));
-    entity.setMass(4);
+    entity.setAcceleration(new Vector2D(0.5,0));
     const renderer = new EntityRenderer(canvas);
 
     renderer.draw(entity);
 
     setInterval(() => {
-        entity.setPosition(entity.getPosition().add(new Vector2D(2,0)));
+        Forces.move(entity);
         renderer.draw(entity);
     }, 10);
 };
--- docs/assets/js/renderers/EntityRenderer.js
+++ docs/assets/js/renderers/EntityRenderer.js
@@ -2,7 +2,6 @@ import {Renderer2D} from "@inwebo/render.js";
 
 export default class EntityRenderer extends Renderer2D{
     _draw([entity]) {
-
         this.getCtx().beginPath();
         this.getCtx().arc(entity.getPosition().getX(), entity.getPosition().getY(), 50, 0, 2 * Math.PI);
         this.getCtx().fillStyle = 'white'
